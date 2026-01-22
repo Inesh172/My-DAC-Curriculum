10856a1dbf8366c86cbb9a6e64444e45d8fea007
Inesh172 <tanaseka001@mymail.sim.edu.sg>
2026-01-22 16:25:55 +0800

    Latest changes

--- DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Revision_Blank.sql
+++ DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Revision_Blank.sql
@@ -11,14 +11,21 @@
 -- humanresources.employee table
 
 --Answer
-
+SELECT *
+FROM humanresources.employee
+ORDER BY jobtitle ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q2: From the following table person.person write a query in SQL to return all rows and a subset of the columns (firstName, lastName, businessentityid) from the person table in the AdventureWorks database. 
 -- The third column heading is renamed to employee_id. Arranged the output in ascending order by lastname.
 
 --Answer
-
+SELECT 
+    firstname, 
+    lastname, 
+    businessentityid AS employee_id
+FROM person.person
+ORDER BY lastname ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q3: From the following table write a query in SQL to return only the rows for product that have a sellstartdate that is not NULL and a productline of 'T'. 
@@ -27,7 +34,15 @@
 -- production.product
 
 --Answer
-
+SELECT 
+    productid, 
+    productnumber, 
+    name
+FROM production.product
+WHERE 
+    sellstartdate IS NOT NULL 
+    AND productline = 'T'
+ORDER BY name ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q4:From the following table write a query in SQL to calculate the total freight paid by each customer. Return customerid and total freight. 
@@ -36,7 +51,12 @@
 -- sales.salesorderheader
 
 --Answer
-
+SELECT 
+    customerid, 
+    SUM(freight) AS total_freight
+FROM sales.salesorderheader
+GROUP BY customerid
+ORDER BY customerid ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q5:From the following table write a query in SQL to retrieve the number of employees for each City. Return city and number of employees. 
@@ -45,7 +65,12 @@
 -- person.businessentityaddress
 
 --Answer
-
+SELECT 
+    city, 
+    COUNT(employeeid) AS number_of_employees
+FROM person.businessentityaddress
+GROUP BY city
+ORDER BY city ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q6: From the following tables write a query in SQL to make a list of contacts who are designated as 'Purchasing Manager'. 
@@ -54,27 +79,55 @@
 -- person.businessentitycontact, person.contacttype, person.person
 
 --Answer
-
+SELECT 
+    person.businessentityid AS BusinessEntityID, 
+    person.lastname, 
+    person.firstname
+FROM person.person AS person
+JOIN person.businessentitycontact AS businessentitycontact
+        ON person.businessentityid = businessentitycontact.PersonID
+JOIN person.contacttype AS contacttype
+        ON businessentitycontact.contacttypeid = contacttype.contacttypeid
+WHERE contacttype.name = 'Purchasing Manager'
+ORDER BY lastname ASC, firstname ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q7: From the following table sales.salesorderdetail  write a query in  SQL to retrieve the total cost of each salesorderID that exceeds 100000. 
 -- Return SalesOrderID, total cost. Round to 2 decimal place and add the dollar sign at the front.
 
 --Answer
+SELECT *
+FROM sales.salesorderdetail;
 
+SELECT 
+    CONCAT("$", ROUND(SUM(unitprice * orderqty),2)) AS total_cost,
+    salesorderid AS SalesOrderID
+FROM sales.salesorderdetail
+GROUP BY salesorderID
+HAVING SUM(unitprice * orderqty) > 100000;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q8:From the following person.person table write a query in  SQL to retrieve those persons whose last name begins with letter 'R' and firstname end with 'n'. 
 -- Return lastname, and firstname and display the result in ascending order on firstname and descending order on lastname columns.
 
 --Answer
-
+SELECT 
+    lastname, 
+    firstname
+FROM person.person
+WHERE
+    lastname LIKE 'R%' 
+    AND firstname LIKE '%n'
+ORDER BY firstname ASC, lastname DESC;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q9: From the following humanresources.department table write a query in  SQL to skip the first 5 rows and return the next 5 rows from the sorted result set.
 
 --Answer
-
+SELECT *
+FROM humanresources.department
+ORDER BY departmentid ASC
+LIMIT 5 OFFSET 5;
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q10:From the following tables write a query in  SQL to find the persons whose last name starts with letter 'L'. 
@@ -83,5 +136,14 @@
 -- person.person, person.personphone
 
 --Answer
-
+SELECT
+    person.businessentityid AS BusinessEntityID,
+    person.firstname AS FirstName,
+    person.lastname AS LastName,
+    personphone.phonenumber AS PhoneNumber
+FROM person.person AS person
+JOIN person.personphone AS personphone
+    ON person.BusinessEntityID = personphone.businessentityid
+WHERE person.lastname LIKE 'L%'
+ORDER BY person.lastname ASC, person.firstname ASC;
 -------------------------------------------------------------------------------------------------------------------------------------------
